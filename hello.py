63dbff63f88c91f3df4db9a413c29a2f632fccf0
Qin <liuqin0821@gmail.com>
2019-11-09 18:22:22 +0800

    test template
    描述：
    思路：
    测试：

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -13,6 +13,12 @@ def hello():
     return 'Nice to meet you!'
 
 
+@app.route('/sign/')
+@app.route('/sign/<name>')
+def sign(name=None):
+    return render_template('test.html', name=name)
+
+
 @app.route('/user/<username>')
 def show_user_profile(username):
     # TypeError: not all arguments converted during string formatting
@@ -22,6 +28,14 @@ def show_user_profile(username):
 @app.route('/user/<int:post_id>')
 def show_post(post_id):
     return 'Post id: %d' % post_id
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'post'
+    else:
+        return 'get'
 
 
 if __name__ == '__main__':
